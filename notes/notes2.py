bd2c9751754ec6631471e8e8c2515024e960b064
joaosucupira <jasucupira@gmail.com>
2025-05-21 03:49:05 +0000

    commit

--- notes/notes2.py
+++ notes/notes2.py
@@ -13,4 +13,3 @@ if cfork == 0:
     print("processo filho")
 else:
     print(f'processo pai, filho {cfork}')
-    https://www.youtube.com/watch?time_continue=56&v=ImfyJ_AC9DY&embeds_referring_euri=https%3A%2F%2Fchatgpt.com%2F&source_ve_path=MTM5MTE3LDEzOTExNywxMjcyOTksMjg2NjY
