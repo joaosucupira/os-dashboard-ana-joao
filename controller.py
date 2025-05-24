ebacee64eebb9e5bfc6eb78bdad420b04e3473ab
joaosucupira <jasucupira@gmail.com>
2025-05-24 20:12:41 +0000

    limpeza model começo de view

--- controller.py
+++ controller.py
@@ -1,7 +1,16 @@
 # Mediador entre model e view 
-
+from model import Model
+from view import DashboardUI
 
 # Classe principal
 class Controller:
     def __init__(self, model, view):
-        pass
+        self.modelo = model
+        self.vista = view
+        self.check_view_model()
+
+        
+    def check_view_model(self):
+        if(self.modelo == None or self.vista == None):
+            print("Sem modelo ou vista") 
+            exit(1)
--- model.py
+++ model.py
@@ -27,55 +27,6 @@ class DiretorioEntrada(ctypes.Structure):
         ("dirOff", ctypes.c_ulong), # offset/deslocamento p proxima entrada
     ]
 
-# Classe implementando uma struct C ... de processos individuais
-
-class EstatisticasProcesso(Structure):
-    _fields_ = [
-        ("pid", c_ulong), # Inteiro sem sinal identificador do processo
-        ("name", c_char * 256), # 
-        ("status", c_char * 64), # Suspensa, Terminada, Executando...
-        ("user", c_char * 128), 
-        ("qtd_threads", c_ulong)
-    ]
-
-# CLASSES PYTHON
-
-@dataclass
-class Processo:
-    pid: int
-    name: str
-    user: str
-
-class Thread:
-    def __init__(self):
-        pass
-
-# class System:
-#     def __init__(self, t_atividade, ram_total, ram_disponivel, n_processos, )
-
-# class System(Structure):
-#     _fields_ = [
-#         ("uptime", c_long), 
-#         ("totalram", c_ulong),
-#         ("freeram", c_ulong),
-#         ("totalprocessos", c_ulong),
-#         ("totalthreads", c_ulong)
-#     ]
-
-class UsoCPU(Structure):
-    _fields_ = [
-        ("usopercent", c_float),
-        ("ociosopercent", c_float)
-
-    ]
-
-class Thread(Structure):
-    _fields_ = [
-        ("TID", c_ulong),
-        ("processoPai", c_ulong),
-        ("status", c_char * 64),
-        ("CPU", c_float)
-    ]
 
 # Classe principal
 class Model:
