720f4afa2fbd0b09bf57dc06c4e0932b37f8444e
joaosucupira <jasucupira@gmail.com>
2025-05-25 05:58:11 +0000

    Colunas da tabela alinhadas

--- Views/TabelaProcessosView.py
+++ Views/TabelaProcessosView.py
@@ -5,14 +5,15 @@ class TabelaProcessosView(ctk.CTk):
         super().__init__()
         self.title("Processos ativos")
         self.geometry("700x500")
-        self.tabela = ctk.CTkTextbox(self, width=650, height=450)
+        self.tabela = ctk.CTkTextbox(self, width=650, height=450, font=("Courier New", 14))
         self.tabela.pack(fill="both", expand=True, padx=10, pady=10)
 
     def mostrar_processos(self, processos):
         self.tabela.delete("0.0", "end")
         header = f"{'PID':<30}{'Usuário':<32}{'Threads':<30}\n"
         self.tabela.insert("end", header)
-        self.tabela.insert("end", "-" * 44 + "\n")
+        self.tabela.insert("end", "-" * 80 + "\n")
         for proc in processos:
+            usuario = str(proc['usuario'])[:31]
             linha = f"{str(proc['pid']):<30}{str(proc['usuario']):<32}{str(proc['threads']):<30}\n"
             self.tabela.insert("end", linha)
