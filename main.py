fc97d8c491bf29ac8e3ab918aadd547789dbd051
joaosucupira <jasucupira@gmail.com>
2025-05-25 04:51:26 +0000

    Listagem de processos e usuario

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Arquivo principal - Dashboard
 # No diretorio raiz rode 'python3 main.py' ou 'python main.py'
 
-def main():
-    pass
+from Models.GerenciadorProcessos import GerenciadorProcessos
 
+def main():
+    gp = GerenciadorProcessos()
+    processos = gp.listar_processos_e_usuarios()
+    for p in processos:
+        print(f'PID:  {p['pid']} - Usuario:  {p['usuario']}')
 if __name__ == "__main__":
     main()
