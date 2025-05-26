f65889d76aebeaed07a28749f7d37f0299a987ef
anamolinos <anamolinos52@gmail.com>
2025-05-26 14:20:58 -0300

    formatando info de memoria

--- Models/GerenciadorMemoria.py
+++ Models/GerenciadorMemoria.py
@@ -28,72 +28,41 @@ class GerenciadorMemoria:
 
         for linha in linhas:
             if "MemTotal" in linha:
-                self.mem_total = int(linha.split()[1])
+                self.mem_total = int(linha.split()[1]) / 1000000
             elif "MemFree" in linha:
-                self.mem_free = int(linha.split()[1])
+                self.mem_free = int(linha.split()[1]) / 1000000
             elif "MemAvailable" in linha:
-                self.mem_available = int(linha.split()[1])
+                self.mem_available = int(linha.split()[1]) / 1000000
             elif "SwapTotal" in linha:
-                self.swap_total = int(linha.split()[1])
+                self.swap_total = int(linha.split()[1]) / 1000000
             elif "SwapFree" in linha:
-                self.swap_free = int(linha.split()[1])
+                self.swap_free = int(linha.split()[1]) / 1000000
             elif "Cached" in linha:
-                self.cached = int(linha.split()[1])
+                self.cached = int(linha.split()[1]) / 1000000
 
         # Dicionario contendo apenas os dados que serao mostrados em tela
         # Para memória física (RAM) e virtual (SWAP):
-        # percentual de uso da memória (real),
-        # quantidade usada,
-        # quantidade total,
-        # percentual de memória livre (real),
+        # - percentual de uso da memória (real),
+        # - quantidade usada,
+        # - quantidade total,
+        # - percentual de memória livre (real),
         # cache
 
         self.dados_memoria = {
             # Dados de memória física (RAM)
-            "mem_total": self.getMemoriaFisicaTotal(),
+            "mem_total": self.mem_total,
             "mem_use_perc": self.calculaPercentualUso(),
             "mem_used": self.calculaMemoriaFisicaUsada(),
+            "mem_available_perc": self.calculaPercentualMemoriaDisponivel(),
             # Dados de memória virtual (SWAP)
-            "swap_total": self.getMemoriaVirtualTotal(),
+            "swap_total": self.swap_total,
             "swap_use_perc": self.calculaPercentualMemoriaVirtualUsada(),
             "swap_used": self.calculaMemoriaVirtualUsada(),
             # Memoria cache
-            "cached": self.getCache()
+            "cached": self.cached
         }
 
         return self.dados_memoria
-
-    
-    def getMemoriaFisicaTotal(self):
-        # Retorna a quantidade total de memória física (RAM) em GB.
-        
-        return self.mem_total / 1000000  # Convertendo de KB para GB
-
-    def getMemoriaFisicaLivre(self):
-        # Retorna a quantidade total de memória física (RAM) livre em GB.
-        
-        return self.mem_free / 1000000
-
-    def getMemoriaFisicaDisponivel(self):
-        # Retorna a quantidade total de memória física (RAM) disponível em GB,
-        # desconsiderando a memoria dedica a buffers e cache.
-        
-        return self.mem_available / 1000000
-
-    def getMemoriaVirtualTotal(self):
-        # Retorna a quantidade total de memória virtual (SWAP) em GB.
-
-        return self.swap_total/ 1000000
-
-    def getMemoriaVirtualLivre(self):
-        # Retorna a quantidade total de memória virtual (SWAP) livre em GB.
-        
-        return self.swap_free / 1000000
-
-    def getCache(self):
-        # Retorna a quantidade total de memória cache em GB.
-        
-        return self.cached / 1000000
 
     def calculaPercentualUso(self):
         # Cálculo do percentual de uso da memória com base em memória disponivel.
@@ -116,19 +85,19 @@ class GerenciadorMemoria:
     def calculaMemoriaFisicaUsada(self):
         # Calcula a quantidade de memória física (RAM) usada em GB.
         
-        return (self.mem_total - self.mem_available) / 1000000
+        return (self.mem_total - self.mem_available)
 
     def calculaMemoriaVirtualUsada(self):
         # Calcula a quantidade de memória virtual (SWAP) usada em GB.
         
-        return (self.swap_total - self.swap_free) / 1000000
+        return (self.swap_total - self.swap_free) * 1000000 # Para expressar em kB
 
     def calculaPercentualMemoriaVirtualUsada(self):
         # Cálculo do percentual de memória virtual usada em GB.
 
         swap_usado = self.calculaMemoriaVirtualUsada()
         if swap_usado > 0:
-            percentual_memoria_virtual_usada = (swap_usado / self.swap_total) * 100
+            percentual_memoria_virtual_usada = (swap_usado / self.swap_total) / 10000 # Esse calculo e referente ao fato de que calculaMemoriaVirtualUsada retorna em kB, e queremos o percentual em GB.
             return percentual_memoria_virtual_usada
         
         return 0.0
--- Views/TabelaMemoriaView.py
+++ Views/TabelaMemoriaView.py
@@ -14,15 +14,12 @@ class TabelaMemoriaView(ctk.CTkToplevel):
         """
         self.tabela.delete("0.0", "end")
         linhas = [
-            f"{'Memória Física Total:':<30}{dados_memoria.get('total', 0):>10.2f} GB\n",
-            f"{'Memória Física Livre:':<30}{dados_memoria.get('livre', 0):>10.2f} GB\n",
-            f"{'Memória Física Disponível:':<30}{dados_memoria.get('disponivel', 0):>10.2f} GB\n",
-            f"{'Memória Cache:':<30}{dados_memoria.get('cache', 0):>10.2f} GB\n",
-            f"{'Swap Total:':<30}{dados_memoria.get('swap_total', 0):>10.2f} GB\n",
-            f"{'Swap Livre:':<30}{dados_memoria.get('swap_livre', 0):>10.2f} GB\n",
-            f"{'Swap Usada:':<30}{dados_memoria.get('swap_usada', 0):>10.2f} GB\n",
-            f"{'Uso de Memória (%):':<30}{dados_memoria.get('uso_percent', 0):>10.2f} %\n",
-            f"{'Uso Real de Memória (%):':<30}{dados_memoria.get('uso_real_percent', 0):>10.2f} %\n",
+            f"Memória Física (RAM):\n",
+            f"{dados_memoria["mem_used"]:.2f} GB ({dados_memoria["mem_use_perc"]:.2f}%) de {dados_memoria["mem_total"]:.2f} GB utilizados\n",
+            f"{dados_memoria["mem_available_perc"]:.2f}% de memória disponível\n",
+            f"Memória cache: {dados_memoria["cached"]:.2f} GB\n",
+            f"\nMemória Virtual (SWAP):\n",
+            f"{dados_memoria["swap_used"]:.2f} kB ({dados_memoria["swap_use_perc"]:.2f}%) de {dados_memoria["swap_total"]:.2f} GB utilizados\n",
         ]
         for linha in linhas:
             self.tabela.insert("end", linha)
