56ee19535f0ce30e9f68971145597f4e763f80e1
joaosucupira <jasucupira@gmail.com>
2025-05-27 14:25:21 +0000

    refatorando gerenciador de processos

--- Models/GerenciadorProcessos.py
+++ Models/GerenciadorProcessos.py
@@ -1,6 +1,7 @@
 from utils.util_diretorio import GerenciadorDiretorio, get_page_size, get_clk_tck, uid_para_nome
 
 class GerenciadorProcessos:
+    
     def listar_processos_e_usuarios(self):
         processos = []
         page_t = get_page_size()
@@ -9,35 +10,24 @@ class GerenciadorProcessos:
         with GerenciadorDiretorio("/proc") as gd:
             for entry in gd:
                 if entry.name.isdigit():
-
                     pid = entry.name
-                    stat_path = f"/proc/{pid}/stat"
+
+                    # caminho para acessar nome, estado, n de threads, tempo de cpu e memoria
+                    stat_path = f"/proc/{pid}/stat" 
+                    # caminho para acessar usuario do processo
                     status_path = f"/proc/{pid}/status"
+
+
                     try:
-                        
-                        with open(stat_path, "r") as fstat:
-                            campos = fstat.read().split()
-                            name = campos[1].strip("()")
-                            estado_id = campos[2]
-                            threads = int(campos[19])
-                            tempo_usuario = int(campos[13])
-                            tempo_sistema = int(campos[14])
-                            total_t = tempo_usuario + tempo_sistema
-                            t_cpu = total_t / clk_tck
-                            rss = int(campos[23])
-                            memoria_kb = (rss * page_t) // 1024
-                        
-                        with open(status_path, "r") as fstatus:
-                            uid = None
-                            for line in fstatus:
-                                if line.startswith("Uid:"):
-                                    uid = line.split()[1]
-                                    break
+                        # Iterando stat
+                        name, estado_id, threads, t_cpu, memoria_kb = self.ler_stat(stat_path, clk_tck, page_t)
+                        # Iterando status
+                        uid = self.ler_uid(status_path)
 
                         if uid is not None:
+                 
                             usuario = uid_para_nome(uid)
                             estado = self.state_id_para_nome(estado_id)
-
                             processos.append({
                                 "pid": pid,
                                 "nome": name,
@@ -48,12 +38,39 @@ class GerenciadorProcessos:
                                 "mem_kb": memoria_kb
                             })
                     except Exception:
-                        continue
-        processos.sort(key=lambda p: (-p['cpu_s'], -p['mem_kb'], int(p['pid'])))        
-        return processos
-    
-    def state_id_para_nome(self, state_id):
+                        continue # ignora todos os diretorios que nao sao processos
 
+        # ordenanado os processos de acordo com 3 critérios principais
+        processos.sort(key=lambda p: (-p['cpu_s'], -p['mem_kb'], int(p['pid'])))        
+        # o retorno será utlizados pelo controller interessado
+        return processos 
+    
+    def ler_stat(self, stat_path, clk_tck, page_t):
+        # Campos referenciando os respectivos índices do arquivo inline que contém o que precisamos
+        with open(stat_path, "r") as fstat:
+            campos = fstat.read().split()
+            name = campos[1].strip("()")
+            estado_id = campos[2]
+            threads = int(campos[19])
+            tempo_usuario = int(campos[13])
+            tempo_sistema = int(campos[14])
+            total_t = tempo_usuario + tempo_sistema
+            t_cpu = total_t / clk_tck
+            rss = int(campos[23])
+            memoria_kb = (rss * page_t) // 1024
+        return name, estado_id, threads, t_cpu, memoria_kb
+
+    def ler_uid(self, status_path):
+        with open(status_path, "r") as fstatus:
+            uid = None
+            for line in fstatus:
+                if line.startswith("Uid:"):
+                    uid = line.split()[1]
+                    break
+        return uid
+
+    # conversão do uid pra o nome do usuario
+    def state_id_para_nome(self, state_id):
         estados = {
             "R": "Executando",
             "S": "Dormindo",
@@ -68,50 +85,5 @@ class GerenciadorProcessos:
             "P": "Parado+",
             "I": "Ocioso"
         }
+        # Pode ocorrer do estado Dormindo ser interpretado incorretamente, verifique o estado no proprio proc
         return estados.get(state_id, "Desconhecido")
-
-    def carregar_detalhes_processo(self, pid):
-        detalhes = {}
-        status_path = f"{pid}/status"
-        stat_path = f"{pid}/stat"
-
-        try:
-            with open(status_path, "r") as f:
-                for line in f:
-                    if line.startswith("Name:"):
-                        detalhes["nome"] = line.split()[1]
-                    if line.startswith("State:"):
-                        detalhes["estado"] = line.split()[1]
-                    if line.startswith("Uid:"):
-                        uid = line.split()[1]
-                        detalhes["usuario"] = uid_para_nome(uid)
-                    if line.startswith("VmRSS:"):
-                        detalhes["memoria_kb"] = int(line.split()[1])
-                    if line.startswith("Threads:"):
-                        detalhes["threads"] = int(line.split()[1])
-        except Exception:
-            detalhes["nome"] = "?"
-            detalhes["estado"] = "?"
-            detalhes["usuario"] = "?"
-            detalhes["memoria_kb"] = 0
-            detalhes["threads"] = 0
-
-        try:
-            with open(stat_path, "r") as f:
-                campos = f.read().split()
-                prioridade = int(campos[17])
-                tempo_usuario = int(campos[13])
-                tempo_sistema = int(campos[14])
-                total_t = tempo_usuario + tempo_sistema
-                
-                clk_tck = get_clk_tck()
-                detalhes["prioridade"] = prioridade
-                detalhes["cpu_s"] = total_t / clk_tck
-
-        except Exception:
-            detalhes["prioridade"] = "?"
-            detalhes["cpu_s"] = 0.0
-
-        detalhes["cpu_percent"] = "N/A" 
-
-        return detalhes
