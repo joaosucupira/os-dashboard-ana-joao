2176417283d4e2c03541c4e7d305555c23b06a52
anamolinos <anamolinos52@gmail.com>
2025-05-28 15:08:34 -0300

    atualizacoes da view e controller da janela de processos

--- Controllers/ProcessosMemoriaController.py
+++ Controllers/ProcessosMemoriaController.py
@@ -1,22 +1,4 @@
 from Models.GerenciadorProcessosMemoria import GerenciadorProcessosMemoria
-from Controllers.DetalhesMemoriaController import DetalhesMemoriaController
-from Views.TabelaProcessosMemoriaView import TabelaProcessosMemoriaView
-
-class ProcessosMemoriaController:
-    def __init__(self, master):
-        self.model = GerenciadorProcessosMemoria()
-        self.view = TabelaProcessosMemoriaView(master=master)
-        self.atualizar_tabela_processos_memoria()
-
-    def atualizar_tabela_processos_memoria(self):
-        dados_processo_memoria = self.model.listar_processos_e_usuarios()
-        self.view.mostrar_processos(dados_processo_memoria)
-
-    def mostrar(self):
-        self.view.deiconify()
-        self.atualizar_tabela_memoria()
-
-    from Models.GerenciadorProcessosMemoria import GerenciadorProcessosMemoria
 from Views.TabelaProcessosMemoriaView import TabelaProcessosMemoriaView
 
 class ProcessosMemoriaController:
